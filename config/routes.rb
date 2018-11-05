32615c3599e2c6ef3f40cf732111cc5d9799c9fe
TranDucTrung <tran.duc.trung@framgia.com>
2018-11-05 10:00:57 +0700

    code skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
     root "home#index"
     devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions",
     passwords: "users/passwords"}, skip: :omniauth_callbacks
+
+    namespace :admin do
+      resources :users
+      get "/dashboards", to: "dashboards#index"
+    end
   end
 end
